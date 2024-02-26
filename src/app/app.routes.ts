5647684b2cde5d16d61caee58ba048ee96fe14f5
wveimar <wveimar.mamian@gmail.com>
2024-02-26 09:07:24 -0500

    Implement HTML templates for Tailwind CSS in home, header, and footer sections (wp: Tailwind layout integration)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'singin',
+        component: SigninComponent
+    },
+    {
+        path: 'singup',
+        component: SignupComponent
+    }
+]
